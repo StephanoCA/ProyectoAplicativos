b5a54fc929bc1e28a304b59fec3f462d84ed22e5
StephanoCA <82909638+StephanoCA@users.noreply.github.com>
2022-07-01 10:41:13 -0500

    Add files via upload

---
+++ ProyectoAplicativos/app/src/main/java/com/upn/proyectoaplicativos/IniciarSinChipActivity.java
@@ -0,0 +1,14 @@
+package com.upn.proyectoaplicativos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IniciarSinChipActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_sin_chip);
+    }
+}
---
+++ ProyectoAplicativos/app/src/main/java/com/upn/proyectoaplicativos/RegistrarActivity.java
@@ -0,0 +1,14 @@
+package com.upn.proyectoaplicativos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+    }
+}
